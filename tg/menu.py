96431327814c11d44356038620aac1b6c4994c61
Elpidio Moreno <elpidiomoreno@gmail.com>
2024-04-09 00:41:31 -0400

    feat (menus): :sparkles: new menus

--- tg/menu.py
+++ tg/menu.py
@@ -6,3 +6,17 @@ main_menu = ReplyKeyboardMarkup([
     [langFile['SEARCHBLOCK'], langFile['STOPSEARCH']],
     [langFile['CONFIGURATIONS']]
 ], resize_keyboard=True)
+
+
+config_menu = ReplyKeyboardMarkup([
+    [langFile['hourlyPay'], langFile['selectHours']],
+    [langFile['selectDays'], langFile['language']],
+    [langFile['backMenu']]
+], resize_keyboard=True)
+
+
+time_menu = ReplyKeyboardMarkup([
+    [langFile['currentStartTime'], langFile['currentEndTime']],
+    [langFile['desiredStartTime'], langFile['desiredEndTime']],
+    [langFile['backConfig']]
+], resize_keyboard=True)
