87305f0a896b3e920991af68adb4232e8626c2b0
Elpidio Moreno <elpidiomoreno@gmail.com>
2024-04-08 22:41:24 -0400

    fix(bot): ✏️ correct uft-8 encodign

--- sis/lang.py
+++ sis/lang.py
@@ -2,4 +2,4 @@ from json import load
 
 from sis.config import configFile
 
-langFile = load(open('langs/{}.json'.format(configFile['lang'])))
+langFile = load(open('langs/{}.json'.format(configFile['lang']), encoding='utf-8'))
